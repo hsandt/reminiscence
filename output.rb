8cc8218df3a6980c42a023accc12044de227b822
hsandt <hs@fuwar.in>
2018-12-12 09:46:19 +0000

    [output] Added wrap_lines

--- output.rb
+++ output.rb
@@ -28,3 +28,48 @@ end
 def compute_reading_time(text_length)
   text_length * READING_TIME_FACTOR  
 end
+
+# return an array of lines compounding passed text
+# where each line contains as many words as
+# possible before max line length is reached
+# fitting spaces at the end of a line are
+# preserved, but leading
+def wrap_lines(text, max_line_length)
+  lines = []
+    # split text into an alternance of words and whitespaces
+  parts = text.split(/(\W+)/)
+  current_line = ""
+  parts.each do |part|
+    if part.strip.empty? then
+      # this part is one or more whitespaces
+      if current_line.length + part.length <= max_line_length then
+        current_line << part
+      else
+        # whitespace is too big but we don't care to display it at the end of the line, so just skip it and finish the line
+        lines << current_line
+        # prepare next line (may remain empty if nothing else after that)
+        current_line = ""
+      end
+    else
+      # this part is a word
+      if current_line.length + part.length <= max_line_length then
+        current_line << part
+      else
+        # if the word is too long but we are at the beginning of the line, it means the wors is longer than
+        if current_line.empty? then
+          current_line << part[0..max_line_length-1]
+        else
+          # finish this line now and start new line with current word
+          lines << current_line
+          current_line = part
+        end
+      end
+    end
+  end
+  # if current line is not empty, add it as the last one
+  unless current_line.empty?
+    lines << current_line
+  end
+  lines
+
+end
--- tc_output.rb
+++ tc_output.rb
@@ -1,10 +1,24 @@
 require_relative "output"
 require "test/unit"
 
-class TestSimpleOutput < Test::Unit::TestCase
+class TestOutput < Test::Unit::TestCase
 
   def test_compute_reading_time
     assert_equal(1, compute_reading_time(20))
+  end
+
+  def test_wrap_lines
+    text = "  this  is  a long  text with  looooooooooong words  and a long               blank  "
+    expected_lines = [
+      "  this  is",
+      "a long  ",
+      "text with",
+      "looooooooo",
+      "words  and",
+      "a long",
+      "blank  "
+    ]
+    assert_equal(expected_lines, wrap_lines(text, 10))
   end
 
 end
